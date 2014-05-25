8d30611cb08b786aef56ffc4b679b4b1a9f7f442
Jeffery Myers <JeffM2501@gmail.com>
2014-05-25 14:43:18 -0700

    Start text IO system

---
+++ ParserTest/IO/ConsoleIO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ParserTest.IO
+{
+	public class ConsoleIO: IInputInterface, IOutputInterface
+	{
+		public List<string> PendingInputLines = new List<string>();
+		
+		public virtual bool HasInputIO()
+		{
+			return PendingInputLines.Count > 0;
+		}
+	}
+}
---
+++ ParserTest/IO/IIOInterfaces.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParserTest.IO
+{
+	interface IInputInterface
+	{
+		bool HasInputIO();
+		string GetInputIOLine();
+	}
+
+	interface IOutputInterface
+	{
+		void OutputIOLine(string line);
+	}
+}
