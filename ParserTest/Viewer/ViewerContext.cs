8a2f4f530f42c098ef9adaff1981a3dd1e21aef4
Jeffery Myers <JeffM2501@gmail.com>
2014-05-26 10:22:25 -0700

    Add viewer

---
+++ ParserTest/Viewer/ViewerContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ParserTest.Describables;
+
+namespace ParserTest.Viewer
+{
+	public class ViewerContext
+	{
+		public List<DescribedElementInstance> Elements = new List<DescribedElementInstance>();
+	}
+}
