e5ae004e69015414baa53d1c307d81010fd7d8b8
Jeffery Myers <JeffM2501@gmail.com>
2014-05-31 21:15:37 -0700

    Break paraser into clearner interfaces

---
+++ MudRoom.IO/CommonInterfaces/ConsoleIO.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace MudRoom.IO.CommonInterfaces
+{
+    public class ConsoleIO
+    {
+        public List<string> PendingInputLines = new List<string>();
+        private Thread ReadThread = null;
+        public ConsoleIO()
+        {
+            ReadThread = new Thread(new ThreadStart(ServiceInput));
+            ReadThread.Start();
+        }
+
+        public void Dispose()
+        {
+            ReadThread.Abort();
+        }
+
+        public void ServiceInput()
+        {
+            StringBuilder line = new StringBuilder();
+            while (true)
+            {
+                bool newline = false;
+
+                if (Console.KeyAvailable)
+                {
+                    char c = Console.ReadKey(true).KeyChar;
+
+                    if (c == '\b')
+                    {
+                        if (line.Length > 0)
+                        {
+                            line.Remove(line.Length - 1, 1);
+                            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                            Console.Write(' ');
+                            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                        }
+                    }
+                    else
+                    {
+                        Console.Write(c);
+                        if (c == '\r' || c == '\n')
+                            newline = true;
+                        else
+                            line.Append(c);
+                    }
+                }
+
+                if (newline)
+                {
+                    lock (PendingInputLines)
+                        PendingInputLines.Add(line.ToString());
+
+                    line.Clear();
+                    Console.WriteLine();
+                }
+
+                if (!Console.KeyAvailable)
+                    Thread.Sleep(10);
+            }
+        }
+
+        public virtual bool HasInputIO()
+        {
+            lock (PendingInputLines)
+                return PendingInputLines.Count > 0;
+        }
+
+        public virtual string GetInputIOLine()
+        {
+            if (!HasInputIO())
+                return string.Empty;
+            lock (PendingInputLines)
+            {
+                string line = PendingInputLines[0];
+                PendingInputLines.RemoveAt(0);
+                return line.Trim();
+            }
+        }
+
+        public virtual void OutputIOLine(string line)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
---
+++ MudRoom.IO/IOInterfaces.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MudRoom.IO
+{
+    public interface IInputInterface
+	{
+		bool HasInputIO();
+		string GetInputIOLine();
+	}
+	public interface IOutputInterface
+	{
+		void OutputIOLine(string line);
+	}
+}
+}
